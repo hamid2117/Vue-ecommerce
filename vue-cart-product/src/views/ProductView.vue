352e6902425e3a463c11b1b99a5af0ebe4703155
hamid2117 <saim008865@gmail.com>
2023-03-21 11:16:41 +0500

    done product page

--- vue-cart-product/src/views/ProductView.vue
+++ vue-cart-product/src/views/ProductView.vue
@@ -3,8 +3,8 @@
     <h1>Products</h1>
 
     <div class="card-container">
-      <div class="card">
-        <div class="card-title">Carrots</div>
+      <div v-for="(product, i) in inventory" :key="product.id" class="card">
+        <div class="card-title">{{ product.name }}</div>
         <div class="card-body">
           <i class="icofont-10x icofont-carrot"></i>
           <form>
@@ -13,199 +13,37 @@
                 <label>Type:</label>
               </div>
               <div class="cell">
-                <em>Vegetable</em>
+                <em>{{ product.type }}</em>
               </div>
             </div>
             <div class="row">
               <div class="cell">
                 <label>Price:</label>
               </div>
-              <div class="cell">$4.82</div>
+              <div class="cell">${{ product.price.USD }}</div>
             </div>
             <div class="row">
               <div class="cell">
                 <label>Quantity:</label>
               </div>
               <div class="cell">
-                <input type="number" value="0" />
+                <input type="number" v-model.number="product.quantity" />
               </div>
             </div>
           </form>
         </div>
         <div class="card-footer">
-          <button class="btn btn-light">Add to cart</button>
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="card-title">Pineapples</div>
-        <div class="card-body">
-          <i class="icofont-10x icofont-pineapple"></i>
-          <form>
-            <div class="row">
-              <div class="cell">
-                <label>Type:</label>
-              </div>
-              <div class="cell">
-                <em>Fruit</em>
-              </div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Price:</label>
-              </div>
-              <div class="cell">$1.62</div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Quantity:</label>
-              </div>
-              <div class="cell">
-                <input type="number" value="0" />
-              </div>
-            </div>
-          </form>
-        </div>
-        <div class="card-footer">
-          <button class="btn btn-light">Add to cart</button>
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="card-title">Cherries</div>
-        <div class="card-body">
-          <i class="icofont-10x icofont-cherry"></i>
-          <form>
-            <div class="row">
-              <div class="cell">
-                <label>Type:</label>
-              </div>
-              <div class="cell">
-                <em>Fruit</em>
-              </div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Price:</label>
-              </div>
-              <div class="cell">$1.04</div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Quantity:</label>
-              </div>
-              <div class="cell">
-                <input type="number" value="0" />
-              </div>
-            </div>
-          </form>
-        </div>
-        <div class="card-footer">
-          <button class="btn btn-light">Add to cart</button>
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="card-title">Carrots</div>
-        <div class="card-body">
-          <i class="icofont-10x icofont-carrot"></i>
-          <form>
-            <div class="row">
-              <div class="cell">
-                <label>Type:</label>
-              </div>
-              <div class="cell">
-                <em>Vegetable</em>
-              </div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Price:</label>
-              </div>
-              <div class="cell">$4.82</div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Quantity:</label>
-              </div>
-              <div class="cell">
-                <input type="number" value="0" />
-              </div>
-            </div>
-          </form>
-        </div>
-        <div class="card-footer">
-          <button class="btn btn-light">Add to cart</button>
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="card-title">Pineapples</div>
-        <div class="card-body">
-          <i class="icofont-10x icofont-pineapple"></i>
-          <form>
-            <div class="row">
-              <div class="cell">
-                <label>Type:</label>
-              </div>
-              <div class="cell">
-                <em>Fruit</em>
-              </div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Price:</label>
-              </div>
-              <div class="cell">$1.62</div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Quantity:</label>
-              </div>
-              <div class="cell">
-                <input type="number" value="0" />
-              </div>
-            </div>
-          </form>
-        </div>
-        <div class="card-footer">
-          <button class="btn btn-light">Add to cart</button>
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="card-title">Cherries</div>
-        <div class="card-body">
-          <i class="icofont-10x icofont-cherry"></i>
-          <form>
-            <div class="row">
-              <div class="cell">
-                <label>Type:</label>
-              </div>
-              <div class="cell">
-                <em>Fruit</em>
-              </div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Price:</label>
-              </div>
-              <div class="cell">$1.04</div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Quantity:</label>
-              </div>
-              <div class="cell">
-                <input type="number" value="0" />
-              </div>
-            </div>
-          </form>
-        </div>
-        <div class="card-footer">
-          <button class="btn btn-light">Add to cart</button>
+          <button @click="addToCart(product.name, i)" class="btn btn-light">
+            add to cart
+          </button>
         </div>
       </div>
     </div>
   </main>
 </template>
+
+<script>
+export default {
+  props: ['inventory', 'addToCart']
+}
+</script>
